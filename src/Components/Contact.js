fafa932a1c30d084aa9b52534c7cb8731902cabb
navjot0 <rohitofficial2001@gmail.com>
2023-02-26 23:10:22 +0530

    Responsive , but theme toggle is not working

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,24 +4,12 @@ const Contact = ({title, tafter}) => {
   return (
     <section className="contact grayBg">
         <div className="container">
-        <h2 className="title" dataText={tafter}>
-                {title}
+        <h2 className="title" dataText={title}>
+                {tafter}
         </h2>
 
         <div className="row">
-            <div className="col-3">
-            <div className="text_side">
-            <h3 className="address">ADDRESS</h3>
-            <p>Lorem ipsum dolor sit amet.  Lorem, ipsum dolor. Lorem ipsum dolor sit amet.j</p>
-            <p><i class="fa-solid fa-phone"></i> {"9897657853"}</p>
-            <p><i class="fa-solid fa-phone"></i> {"667677853"}</p>
-            <p><i class="fa-solid fa-envelope"></i> auu@gmail.com</p>
-            <h3>FOLLOW ME</h3>
-            <div className="socials"><i class="fa-brands fa-github"></i> <i class="fa-brands fa-linkedin"></i> <i class="fa-brands fa-instagram"></i> <i class="fa-brands fa-twitter"></i> <i class="fa-brands fa-google"></i></div>
-            </div>
-            </div>
-
-        <div class="col-9 col-mb-12">
+        <div class="col-8 col-mb-12">
           <h3>SEND US A NOTE</h3>
           <form action="" class="contact__form">
             <div class="row">
@@ -40,6 +28,18 @@ const Contact = ({title, tafter}) => {
       <button className='Btn Btn--primary Btn--rounded Btn--shadow mrtop25'>Send Message</button>
         
         </div>
+            <div className="col-4 col-mb-12">
+            <div className="text_side">
+            <h3 className="address">ADDRESS</h3>
+            <p>Lorem ipsum dolor sit amet.  Lorem, ipsum dolor. Lorem ipsum dolor sit amet.j</p>
+            <p><i class="fa-solid fa-phone"></i> {"(+91) 9897657853"}</p>
+            <p><i class="fa-solid fa-phone"></i> {"(+91) 667677853"}</p>
+            <p><i class="fa-solid fa-envelope"></i> auu@gmail.com</p>
+            <h3>FOLLOW ME</h3>
+            <div className="socials"><i class="fa-brands fa-github"></i> <i class="fa-brands fa-linkedin"></i> <i class="fa-brands fa-instagram"></i> <i class="fa-brands fa-twitter"></i> <i class="fa-brands fa-google"></i></div>
+            </div>
+            </div>
+
         </div>
         </div>
     </section>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,29 @@
-import React, {useRef, useState} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 
 const Header = () => {
-  const myRef = useRef(null);
   const [cross, handleCross] = useState(false)
+
+  const [windowSize, setWindowSize] = useState(
+    window.innerWidth);
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowSize(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleWindowResize);
+
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  });
   return (
     <header id="myHeader">
                 <div className="logo_part">
                  <span className='pfp'><img  src="https://avatars.githubusercontent.com/u/58967842?v=4" alt="Rohit's Image" /></span> 
                  <a href="/"><h1>Rohit Kumar</h1></a>
                 </div>
-               <div className="links">
+               {windowSize < 770 && <div className="links"> 
                 <nav>    
                         <div className={cross ? "toggle_btn cross": "toggle_btn"} onClick={()=>handleCross(!cross)} ><div className="lines"></div><div className="lines"></div><div className="lines"></div></div>
                         <ul className="navigation">
@@ -37,7 +51,38 @@ const Header = () => {
                     <i className="fa-brands fa-snapchat"></i>
                   </a>
                 </ul>
-                </div>
+                </div> }
+                
+                {windowSize >= 770 && <>
+                <nav>    
+                        <div className={cross ? "toggle_btn cross": "toggle_btn"} onClick={()=>handleCross(!cross)} ><div className="lines"></div><div className="lines"></div><div className="lines"></div></div>
+                        <ul className="navigation">
+                            <li><a href="#home">Home</a></li>
+                            <li><a href="#about">About Me</a></li>
+                            <li><a href="#whatido">What I DO</a></li>
+                            <li><a href="resume">Resume</a></li>
+                            <li><a href="portfolio">Portfolio</a></li>
+                            <li><a href="testimonial">Testimonial</a></li>
+                            <li><a href="contact">Contact</a></li>
+                        </ul>
+                </nav>
+              
+                <ul className="socials">
+                  <a href="">
+                    <i className="fa-brands fa-instagram"></i> 
+                  </a>
+                  <a href="">
+                    <i className="fa-brands fa-github"></i>
+                  </a>
+                  <a href="">
+                    <i className="fa-brands fa-linkedin"></i>
+                  </a>
+                  <a href="">
+                    <i className="fa-brands fa-snapchat"></i>
+                  </a>
+                </ul>
+                </>
+               }
 
             
     </header>
